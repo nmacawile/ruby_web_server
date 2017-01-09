bb9e5d9bc368673d88b44b8c57e13c4ebbd873db
nmacawile <nmacawile@gmail.com>
2017-01-09 17:38:04 +0800

    added request parser on server

--- lib/browser.rb
+++ lib/browser.rb
@@ -2,21 +2,40 @@ require 'socket'
  
 host = 'localhost'     				# The web server
 port = 2000                          # Default HTTP port
-path = "/index.html"          			# The file we want 
+#path = "/index.html"          			# The file we want 
+
+def generate_get_request(path)
+	"GET #{path} HTTP/1.0\r\n"\
+	"From: someone@emaildomain.com\r\n"\
+	"User-Agent: HTTPTool/1.0\r\n\r\n"
+end
+
+def generate_post_request(path, json_data)
+	"POST #{path} HTTP/1.0\r\n"\
+	"From: frog@jmarshall.com\r\n"\
+	"User-Agent: HTTPTool/1.0\r\n"\
+	"Content-Type: application/x-www-form-urlencoded\r\n"\
+	"Content-Length: #{json_data.size}\r\n\r\n"
+end
 
 
 puts "What do you want to do?: "
 puts "[G] visit the viking main page"
 puts "[P] register a viking for raid"
-gets "> "
+print "> "
 
+action = gets.chomp.upcase
 # This is the HTTP request we send to fetch a file
-request = 	"GET #{path} HTTP/1.0\r\n"\
-			"From: someone@emaildomain.com\r\n"\
-			"User-Agent: HTTPTool/1.0\r\n\r\n"
+request = nil
+if action == "G"
+	request = generate_get_request("/index.html")
+elsif action == "P"
+	request = generate_post_request("/index.html")
+end
 #puts request
 
-socket = TCPSocket.open(host,port)  # Connect to server
+socket = TCPSocket.open(host, port)  # Connect to server
+
 socket.print(request)               # Send request
 response = socket.read              # Read complete response
 puts response						# Print complete response
--- lib/server.rb
+++ lib/server.rb
@@ -2,23 +2,65 @@ require 'socket'               	# Get sockets from stdlib
 
 server = TCPServer.open(2000)  	# Socket to listen on port 2000
 
+def generate_get_response(path)
+	file = File.new("..#{path}")
+	response = 
+		"HTTP/1.0 200 OK\r\n"\
+		"Date: #{Time.now.ctime}\r\n"\
+		"Content-Type: text/html\r\n"\
+		"Content-Length: #{file.size}\r\n\r\n"	
+	file.readlines.each { |line| response += line }
+	file.close
+	response += "\r\n\r\n"
+	response
+end
+
+def generate_post_response(path, data)
+
+end
+
+class Request
+	attr_reader :type, :path, :version, :headers
+
+	def initialize(type, path, version, headers)
+		@type = type
+		@path = path
+		@version = version
+		@headers = headers
+	end
+
+	def self.parse(string)
+		initial_line, *header_strings = string.scan(/^(.+)(?:\r\n)+/).flatten
+		type, path, version = initial_line.split(/\s/, 3)
+		headers = {}
+		header_strings.each do |string| 
+			splitted = string.split(": ", 2)
+			headers[splitted[0]] = splitted[1]
+		end
+		self.new(type, path, version, headers)
+	end
+end
+
 loop {                        	 # Servers run forever
   	client = server.accept       # Wait for a client to connect
 
-  	while request = client.gets and request !~ /^\s+$/
-  		puts request # Prints whatever the client enters on the server's output
+  	request_string = ""
+
+  	while request_line = client.gets and request_line !~ /^\s+$/
+  		request_string += request_line # Prints whatever the client enters on the server's output
   	end
 
-	file = File.new("../index.html")
+  	#puts request_string
 
- 	client.print 	"HTTP/1.0 200 OK\r\n"\
-					"Date: #{Time.now.ctime}\r\n"\
-					"Content-Type: text/html\r\n"\
-					"Content-Length: #{file.size}\r\n\r\n"
+  	request = Request.parse(request_string)
+  	puts "type: #{request.type}"
+  	puts "path: #{request.path}"
+  	puts "version: #{request.version}"
+  	puts "headers: #{request.headers}"
 
-	file.readlines.each { |line| client.print line }
-	client.print "\r\n\r\n"
 
- 	file.close
+
+ 	client.print generate_get_response("/index.html")
+
   	client.close			      # Disconnect from the client
 }
